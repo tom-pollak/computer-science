a1a63fbdd6adaae9d821c8f5fabcc4b1d870d432
Tom Pollak <tompollak1000@gmail.com>
2021-10-21 12:39:28 +0100

    update

---
+++ int1/wk_4.py
@@ -0,0 +1,107 @@
+class Castle:
+    """Castle node in map"""
+
+    def __init__(self, name, height):
+        self.name = name
+        self.height = height
+        self.castles = {}
+        self.straight_line_dist = {}
+
+    def generate_arcs(self, graph):
+        for i, arc in enumerate(graph[self][0]):
+            if arc in [0, None]:
+                continue
+            castle = list(graph)[i]
+            self.castles[castle] = arc
+
+        # self.castles = {
+        #     k: v for k, v in sorted(self.castles.items(), key=lambda x: x[1])
+        # }
+
+    def generate_heuristics(self, heuristic):
+        for i, dist in enumerate(heuristic[self]):
+            castle = list(heuristic)[i]
+            self.straight_line_dist[castle] = dist
+
+    def bfs(self, goal, queue=None, visited=None, dist_from_home=0):
+        print(self.name, dist_from_home)
+        best_dist = None
+        if queue is None:
+            queue = []
+        if visited is None:
+            visited = []
+        visited.append(self)
+
+        while True:
+            if self.name == goal:
+                return dist_from_home
+
+            for connected_castle, arc in zip(self.castles, self.castles.values()):
+                if connected_castle in visited:
+                    continue
+                time_to_walk = (dist_from_home + arc) / 5 + (
+                    self.height - connected_castle.height
+                ) / 600
+
+                try:
+                    c_index = [c[0] for c in queue].index(connected_castle)
+                except ValueError:
+                    queue.append([connected_castle, time_to_walk])
+                    continue
+                queue[c_index][1] = min(queue[c_index][1], time_to_walk)
+            if len(queue) == 0:
+                return best_dist
+
+            queue.sort(key=lambda x: x[1])
+            next_castle, arc = queue.pop(0)
+            dist = next_castle.bfs(goal, queue, visited, arc)
+
+            if dist is None:
+                pass
+            elif best_dist is None or dist < best_dist:
+                best_dist = dist
+        return best_dist
+
+
+heuristic = {
+    "A": [0, 3, 3, 3, 7, 7, 11, 11, 15, 15],
+    "B": [3, 0, 7, 3, 7, 3, 7, 11, 11, 15],
+    "C": [3, 7, 0, 3, 3, 11, 15, 7, 15, 15],
+    "D": [3, 3, 3, 0, 3, 7, 7, 7, 15, 15],
+    "E": [7, 7, 3, 3, 0, 11, 7, 3, 11, 11],
+    "F": [7, 3, 11, 7, 11, 0, 3, 7, 3, 7],
+    "G": [11, 7, 15, 7, 7, 3, 3, 3, 3],
+    "H": [11, 11, 7, 7, 3, 7, 3, 0, 7, 3],
+    "I": [15, 11, 15, 11, 7, 3, 3, 7, 0, 3],
+    "J": [15, 15, 15, 15, 11, 7, 3, 3, 3, 0],
+}
+
+graph = {
+    "A": [[0, 5, 5, 5, None, None, None, None, None, None], 0],
+    "B": [[5, 0, None, None, None, 5, None, None, None, None], 200],
+    "C": [[5, None, 0, None, 5, None, None, None, None, None], 600],
+    "D": [[5, None, None, 0, 5, 10, None, None, None, None], 100],
+    "E": [[None, None, 5, 5, 0, None, 10, 5, None, None], 300],
+    "F": [[None, 5, None, 10, None, 0, None, None, 5, None], 300],
+    "G": [[None, None, None, None, 10, None, 0, 5, None, 5], 100],
+    "H": [[None, None, None, None, 5, None, 5, 0, None, 5], 500],
+    "I": [[None, None, None, None, None, 5, None, None, 0, 5], 400],
+    "J": [[None, None, None, None, None, None, 5, 5, 5, 0], 500],
+}
+
+castles = []
+
+for castle, height in zip(list(graph.keys()), [x for _, x in list(graph.values())]):
+    new_castle = Castle(castle, height)
+    castles.append(new_castle)
+
+graph = {k: v for k, v in zip(castles, graph.values())}
+heuristic = {k: v for k, v in zip(castles, heuristic.values())}
+
+for castle in graph:
+    castle.generate_arcs(graph)
+    castle.generate_heuristics(heuristic)
+
+
+A = list(graph)[0]
+print(A.bfs("J"))
--- sys2/simple_fork.c
+++ sys2/simple_fork.c
@@ -16,13 +16,11 @@ return_pid = fork(); //first child created
 if (return_pid == -1) {      /* fork() failed */
     perror("fork");
     exit(1);
-} else if (return_pid == 1) {
-    printf("Parent process = 1");
 }
-printf("The main process %d \n",pid_main);
-printf("The parent process of the current process is %d \n",getppid());
-printf("The child return pid %d \n", return_pid);
-printf("The current process %d \n",getpid());
+/* printf("The main process %d \n",pid_main); */
+printf("The parent process of the current process %d is %d \n", getpid(), getppid());
+/* printf("The child return pid %d \n", return_pid); */
+/* printf("The current process %d \n",getpid()); */
 
 if(return_pid == 0)
 {
@@ -31,9 +29,9 @@ if(return_pid == 0)
 }
 else
 {
-    /* printf("I am the main parent process"); */
+    printf("I am the main parent process\n\n");
     /* sleep(10); */
     terminated_child_pid = wait(NULL);
-    printf("The terminated child %d \n", terminated_child_pid); 
+    printf("The terminated child %d \n", terminated_child_pid);
 
 }}
